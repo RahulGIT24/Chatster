61c55d5a58de2831bf3a8ef8c80b2cf91e6b01e2
Rahul <rg4005450@gmail.com>
2023-08-27 20:28:04 +0530

    Fixed a typo

--- Backend/server.js
+++ Backend/server.js
@@ -43,7 +43,7 @@ const server = app.listen(port, console.log("Listening on port " + port));
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: process.env.path(PATH)
+        origin: process.env.PATH
     }
 })
 
