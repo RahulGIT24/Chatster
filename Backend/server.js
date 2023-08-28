5b9ef8ad5a9aa46ffc05108f4e0d14043a451a3a
Rahul <rg4005450@gmail.com>
2023-08-28 18:15:23 +0530

    Changed socket.io origin to *

--- Backend/server.js
+++ Backend/server.js
@@ -43,7 +43,7 @@ const server = app.listen(port, console.log("Listening on port " + port));
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "https://chatster.onrender.com/"
+        origin: "*"
     }
 })
 
